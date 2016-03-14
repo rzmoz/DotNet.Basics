36b10a41c903b3ac09a23858bbedfe14bdefe703
rzmoz <rzmoz@users.noreply.github.com>
2016-03-14 14:25:44 +0800

    Added and updated console args parser func

---
+++ DotNet.Basics/ConsoleApp/CmdLine.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNet.Basics.ConsoleApp
+{
+    public class CmdLine
+    {
+        private const char _quoteChar = '\"'; //quote
+        private readonly Dictionary<string, CmdLineParam> _parameters = new Dictionary<string, CmdLineParam>();
+
+        public string this[string key]
+        {
+            get
+            {
+                if (!_parameters.ContainsKey(key))
+                    throw new KeyNotFoundException(key);
+                return _parameters[key]?.Value?.Trim(_quoteChar);
+            }
+        }
+
+        public bool Exists(string key) => _parameters[key].Exists;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="paramName"></param>
+        /// <param name="required"></param>
+        /// <param name="allowEmpty"></param>
+        /// <param name="readAction">action where you can access the parsed argument - it's run on Parse()</param>
+        /// <returns></returns>
+        public CmdLine Register(string paramName, bool required, bool allowEmpty, Action<CmdLineParam> readAction)
+        {
+            return Register(new CmdLineParam(paramName) { Required = required, AllowEmptyValue = allowEmpty }, readAction);
+        }
+
+        public CmdLine Register(CmdLineParam parameter, Action<CmdLineParam> readAction)
+        {
+            var key = parameter.Name.ToLower();
+            if (_parameters.ContainsKey(key))
+                throw new ArgumentException($"Parameter '{key}' is already registered.", key);
+
+            parameter.ReadAction = readAction;
+            _parameters.Add(key, parameter);
+            return this;
+        }
+
+        public bool Remove(string name)
+        {
+            var key = name.ToLower();
+            if (_parameters.ContainsKey(key))
+                return _parameters.Remove(key);
+            return false;
+        }
+
+        public void ClearParameters()
+        {
+            _parameters.Clear();
+        }
+
+        public bool Parse(string[] args)
+        {
+            string error = string.Empty;
+            try
+            {
+                ParseArgs(args);
+            }
+            catch (ArgumentException ex)
+            {
+                error = ex.Message;
+            }
+
+            if (error != string.Empty)
+            {
+                Console.WriteLine(Environment.NewLine + error);
+                Console.WriteLine(HelpScreen());
+                return false;
+            }
+
+            //run read actions
+            foreach (var cmdLineParam in _parameters)
+            {
+                cmdLineParam.Value.ReadAction.Invoke(cmdLineParam.Value);
+            }
+
+            return true;
+        }
+
+        private void ParseArgs(string[] args)
+        {
+            int argsPointer = 0;
+
+            while (argsPointer < args.Length)
+            {
+                var arg = args[argsPointer];
+
+                if (IsParameter(arg))
+                {
+                    string key = arg.TrimStart('-').ToLower();
+                    string value = string.Empty;
+                    argsPointer++;
+
+                    bool nextIsValue = true;
+
+                    while (nextIsValue && argsPointer < args.Length)
+                    {
+                        arg = args[argsPointer];
+                        if (IsParameter(arg))
+                        {
+                            nextIsValue = false;
+                        }
+                        else
+                        {
+                            // The next string is a value, read the value and move forward
+                            value += "|" + arg;
+                            argsPointer++;
+                        }
+                    }
+                    if (!_parameters.ContainsKey(key))
+                        throw new ArgumentException("Parameter is not recognized.", key);
+
+                    if (_parameters[key].Exists)
+                        throw new ArgumentException("Parameter is specified more than once.", key);
+
+                    _parameters[key].SetValue(value.Trim('|'));
+                }
+                else
+                {
+                    argsPointer++;
+                }
+            }
+
+            CheckRequiredParametersArePresent();
+            CheckParametersHaveValues();
+        }
+
+        private void CheckParametersHaveValues()
+        {
+            foreach (var cmdLineParameter in _parameters.Values)
+            {
+                if (cmdLineParameter.AllowEmptyValue || !cmdLineParameter.Exists)
+                    continue;
+                if (string.IsNullOrEmpty(cmdLineParameter.Value))
+                    throw new ArgumentException("Value is empty.", cmdLineParameter.Name);
+            }
+        }
+
+        private void CheckRequiredParametersArePresent()
+        {
+            foreach (string key in _parameters.Keys)
+            {
+                if (_parameters[key].Required && !_parameters[key].Exists)
+                    throw new ArgumentException("Required parameter is not found.", key);
+            }
+        }
+
+        private bool IsParameter(string s)
+        {
+            return s.Length > 0 && s[0] == '-';
+        }
+
+        public string HelpScreen()
+        {
+            int len = _parameters.Keys.Select(key => key.Length).Concat(new[] { 0 }).Max();
+
+            string help = "\nParameters:\r\n\r\n";
+            foreach (var parameter in _parameters.Values)
+            {
+                string s = "-" + parameter.Name;
+                while (s.Length < len + 3)
+                    s += " ";
+                if (parameter.Required)
+                    s += "<Required> ";
+                s += parameter.Help + Environment.NewLine;
+                help += s;
+            }
+            return help;
+        }
+
+    }
+}
---
+++ DotNet.Basics/ConsoleApp/CmdLineParam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DotNet.Basics.ConsoleApp
+{
+    public class CmdLineParam
+    {
+        public CmdLineParam(string name)
+        {
+            Name = name;
+            Help = string.Empty;
+            Required = true;
+            AllowEmptyValue = false;
+            Value = string.Empty;
+            Exists = false;
+            ReadAction = param => { };
+        }
+
+        public void SetValue(string value)
+        {
+            Value = value;
+            Exists = true;
+        }
+
+        public string Value { get; private set; }
+        public string Help { get; set; }
+        public bool Exists { get; private set; }
+        public bool Required { get; set; }
+        public bool AllowEmptyValue { get; set; }
+        public string Name { get; private set; }
+        internal Action<CmdLineParam> ReadAction { get; set; }
+    }
+}
