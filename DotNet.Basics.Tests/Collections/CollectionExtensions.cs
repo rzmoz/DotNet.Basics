17fe646631a0812349afdf82b1ee7f80a07907db
rzmoz <rzmoz@users.noreply.github.com>
2016-04-14 13:40:26 +0800

    added foreach to collections

---
+++ DotNet.Basics.Tests/Collections/CollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DotNet.Basics.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.Collections
+{
+    [TestFixture]
+    public class CollectionExtensions
+    {
+        [Test]
+        public void ForEach_SimpleAction_ActionIsAppliedToallElementsInCol()
+        {
+            const int expected = 2;
+
+            var ones = new[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+            var twos = ones.ForEach(one => one = expected);
+
+            foreach (var two in twos)
+                two.Should().Be(expected);
+        }
+    }
+}
---
+++ DotNet.Basics/Collections/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNet.Basics.Collections
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> col, Func<T, T> forEachAction)
+        {
+            return col.Select(forEachAction).ToList();//to list is important to run in order to invoke the action
+        }
+    }
+}
