a1d16ea9b38e73f46a038b4549e5a6e2f2cfc203
rzmoz <rzmoz@users.noreply.github.com>
2016-04-15 19:35:45 +0800

    Added mediator diagnostics to enable multiple loggers

---
+++ DotNet.Basics.Tests/Diagnostics/MediatorDiagnosticsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using DotNet.Basics.Collections;
+using DotNet.Basics.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.Diagnostics
+{
+    [TestFixture]
+    public class MediatorDiagnosticsTests
+    {
+        [Test]
+        public void Log_InMemDiagnostics_AllLoggersAreLoggedTo()
+        {
+            var runs = Enumerable.Range(0, 7); //random prime
+            var mediator = new MediatorDiagnostics();
+            var loggers = new List<InMemDiagnostics>();
+            var logLevel = LogLevel.Debug;
+            runs.ForEach(i => loggers.Add(new InMemDiagnostics())); //add loggers
+            runs.ForEach(i => mediator.Add(i.ToString(), loggers[i])); //add loggers
+
+            loggers.ForEach(l => l.GetLogs(logLevel).Count.Should().Be(0));//ensure loggers are empty
+
+            //act
+            runs.ForEach(i => mediator.Log(i.ToString(), logLevel));//log
+
+            //assert
+            loggers.ForEach(l => l.GetLogs(logLevel).Count.Should().Be(runs.Count()));
+        }
+    }
+}
---
+++ DotNet.Basics/Diagnostics/MediatorDiagnostics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet.Basics.Diagnostics
+{
+    public class MediatorDiagnostics : IDiagnostics
+    {
+        private ConcurrentDictionary<string, IDiagnostics> _loggers;
+
+        public MediatorDiagnostics()
+        {
+            _loggers = new ConcurrentDictionary<string, IDiagnostics>();
+        }
+
+        public void Add(string name, IDiagnostics logger)
+        {
+            _loggers.AddOrUpdate(name, logger, (n, l) => l);
+        }
+
+        public void Clear()
+        {
+            Interlocked.Exchange(ref _loggers, new ConcurrentDictionary<string, IDiagnostics>());
+        }
+
+        public void Log(string message, LogLevel logLevel = LogLevel.Info, Exception exception = null)
+        {
+            Parallel.ForEach(_loggers, logger => logger.Value.Log(message, logLevel, exception));
+        }
+
+        public void Metric(string name, double value)
+        {
+            Parallel.ForEach(_loggers, logger => logger.Value.Metric(name, value));
+        }
+
+        public void Profile(string taskName, TimeSpan duration)
+        {
+            Parallel.ForEach(_loggers, logger => logger.Value.Profile(taskName, duration));
+        }
+    }
+}
