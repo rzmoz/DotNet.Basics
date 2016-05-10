f46756b9c5aeaf8db74a1a2576c7376c5737d9f4
rzmoz <rzmoz@users.noreply.github.com>
2016-05-10 12:47:15 +0800

    fixed to path so you can set delimiter - deleted uri combine since it was the same

---
+++ DotNet.Basics.Tests/IO/PathExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNet.Basics.IO;
+using DotNet.Basics.Sys;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.IO
+{
+    [TestFixture]
+    public class PathExtensions
+    {
+        [Test]
+        [TestCase(PathDelimiter.Slash)]
+        [TestCase(PathDelimiter.Backslash)]
+        public void ToPath_PathDelimiter_DelimiterIsSet(PathDelimiter pathDelimiter)
+        {
+            var pathPt1 = "pt1";
+            var pathPt2 = "pt2";
+
+            var path = pathPt1.ToPath(pathPt2).WithDelimiter(pathDelimiter);
+
+            var delimiter = '/';
+            if (pathDelimiter == PathDelimiter.Backslash)
+                delimiter = '\\';
+
+            path.Should().Be(pathPt1 + delimiter + pathPt2);
+        }
+    }
+}
--- DotNet.Basics/IO/PathExtensions.cs
+++ DotNet.Basics/IO/PathExtensions.cs
@@ -1,11 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using DotNet.Basics.Sys;
 
 namespace DotNet.Basics.IO
 {
     public static class PathExtensions
     {
+        private const char _slashDelimiter = '/';
+        private const char _backslashDelimiter = '\\';
+
+        public static string WithDelimiter(this string path, PathDelimiter pathDelimiter = PathDelimiter.Backslash)
+        {
+            if (path == null)
+                return null;
+            switch (pathDelimiter)
+            {
+                case PathDelimiter.Backslash:
+                    return path.Replace(_slashDelimiter, _backslashDelimiter);
+                case PathDelimiter.Slash:
+                    return path.Replace(_backslashDelimiter, _slashDelimiter);
+                default:
+                    throw new NotSupportedException($"Path Delimiter not supported:{pathDelimiter}");
+            }
+        }
+
         public static string ToPath(this IEnumerable<string> paths)
         {
             var pathsList = paths.Select(CleanPath).ToArray();
@@ -36,7 +56,7 @@ namespace DotNet.Basics.IO
         {
             if (string.IsNullOrWhiteSpace(rawPath))
                 return string.Empty;
-            return rawPath.Trim('/').Trim('\\');
+            return rawPath.Trim(_slashDelimiter).Trim(_backslashDelimiter).Trim(_slashDelimiter);
         }
     }
 }
