3d2cb1d7e92644f5a7c128795966497211e1a7ea
rzmoz <rzmoz@users.noreply.github.com>
2016-05-13 10:56:22 +0800

    Added debug logger for unit tests where classes uses ILogger

---
+++ DotNet.Basics/Diagnostics/DebugLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNet.Basics.Diagnostics
+{
+    /// <summary>
+    /// Logs to Debug. Only useful in local debug and testing scenarios as code needs to run in debug compile
+    /// </summary>
+    public class DebugLogger : DotNetBasicsLogger
+    {
+        protected override void Log(LogEntry entry)
+        {
+            Debug.WriteLine($"{entry.Level}: {entry.Timestamp.ToString("yyyy/MM/dd_hh:mm:ss")} - {entry.Message}{Environment.NewLine}{entry.Exception}");
+        }
+    }
+}
