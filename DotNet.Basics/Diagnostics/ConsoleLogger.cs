0141f27e2ca30070f2252d077366dee98e858139
rzmoz <rzmoz@users.noreply.github.com>
2016-05-24 14:19:57 +0200

    added console logger for convenience

---
+++ DotNet.Basics/Diagnostics/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNet.Basics.Diagnostics
+{
+    public class ConsoleLogger : DotNetBasicsLogger
+
+    {
+        protected override void Log(LogEntry entry)
+        {
+            Console.WriteLine($"{entry.Level}: {entry.Timestamp.ToString("yyyy/MM/dd_hh:mm:ss")} - {entry.Message}{Environment.NewLine}{entry.Exception}");
+        }
+    }
+}
