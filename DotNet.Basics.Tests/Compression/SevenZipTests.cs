488ee03a8e8c79e25ee0461a7ea07f113a771938
rzmoz <rzmoz@users.noreply.github.com>
2016-06-15 16:15:39 +0800

    Added 7zip as inline tool

---
+++ DotNet.Basics.Tests/Compression/SevenZipTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using DotNet.Basics.Compression;
+using DotNet.Basics.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.Compression
+{
+    [TestFixture]
+    public class SevenZipTests
+    {
+        [Test]
+        public void CreateFromDirectory_DontOverWrite_ExceptionWhenTargetAlreadyExists()
+        {
+            //arrange
+            var targetPath = "CreateFromDirectory_DontOverWrite_ExceptionWhenTargetAlreadyExists".ToPath("myArchive.zip");
+            targetPath.DeleteIfExists();
+            "dummyContent".WriteAllText(targetPath);
+            targetPath.Exists().Should().BeTrue();
+            var zip = new SevenZip();
+            //act
+            Action action = () => zip.CreateFromDirectory("mySource", targetPath.FullName, false);
+
+            action.ShouldThrow<IOException>();
+        }
+
+        [Test]
+        public void CreateFromDirectory_Zip_ContentIsZipped()
+        {
+            //arrange
+            var sourceDir = "CreateFromDirectory_Zip_ContentIsZipped".ToPath(DetectOptions.SetToDir, "source");
+            var dummyfile = sourceDir.Add("myfile.txt");
+            var dummycontent = "dummyContent";
+
+            dummycontent.WriteAllText(dummyfile);
+
+            var targetZip = "CreateFromDirectory_Zip_ContentIsZipped".ToPath("myArchive.zip");
+            targetZip.DeleteIfExists();
+
+            targetZip.Exists().Should().BeFalse();
+            var zip = new SevenZip();
+            //act
+            zip.CreateFromDirectory(sourceDir.FullName, targetZip.FullName);
+
+            targetZip.Exists().Should().BeTrue($"Exists:{targetZip.FullName}");
+            using (var archive = ZipFile.OpenRead(targetZip.FullName))
+            {
+                archive.Entries.Count.Should().Be(1);
+
+                using (var stream = archive.Entries.Single().Open())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var read = reader.ReadToEnd();
+                    read.Should().Be(dummycontent);
+                }
+            }
+        }
+
+        [Test]
+        public void ExtractToDirectory_TargetDirDoesntExist_ArchiveIsExtractedToNewDir()
+        {
+            //arrange
+            var targetDir = "ExtractToDirectory_TargetDirDoesntExist_ArchiveIsExtractedToNewDir".ToPath(DetectOptions.SetToDir, "out");
+            var targetFile = targetDir.Add(DetectOptions.SetToFile, "myfile.txt");
+            var sourceZip = "compression".ToPath("myArchive.zip");
+            targetDir.DeleteIfExists();
+            targetDir.Exists().Should().BeFalse();
+            targetFile.Exists().Should().BeFalse();
+
+            var zip = new SevenZip();
+            //act
+            zip.ExtractToDirectory(sourceZip.FullName, targetDir.FullName);
+            targetDir.Exists().Should().BeTrue($"Exists:{targetDir.FullName}");
+            targetDir.ToDir().EnumerateFileSystemInfos().Count().Should().Be(1);
+            targetFile.Exists().Should().BeTrue();
+
+            var content = File.ReadAllText(targetFile.FullName);
+
+            content.Should().Be("dummyContent");
+        }
+    }
+}
---
+++ DotNet.Basics/Compression/SevenZip.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+using DotNet.Basics.IO;
+using DotNet.Basics.Sys;
+using Microsoft.Extensions.Logging;
+
+namespace DotNet.Basics.Compression
+{
+    public class SevenZip
+    {
+        private readonly ILogger _logger;
+
+        public SevenZip(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        public int ExtractToDirectory(string archivePath, string targetDirPath)
+        {
+            return ExecuteSevenZip("x", $"\"{archivePath}\"", $"\"-o{targetDirPath.ToPath(DetectOptions.SetToDir)}\"", "*", "-r");
+        }
+
+        public int CreateFromDirectory(string sourceDirPath, string archivePath, bool overwrite = false)
+        {
+            if (overwrite == false && archivePath.ToPath().Exists())
+                throw new IOException($"Target archie path already exists: {archivePath}. Set overwrite to true to ignore");
+
+            return ExecuteSevenZip("a", $"\"{archivePath}\"", $"\"{sourceDirPath.ToPath(DetectOptions.SetToDir)}\\*\"", "-tzip", "-mx3", "-mmt");
+        }
+
+        private int ExecuteSevenZip(string command, params string[] @params)
+        {
+            using (var temp = new TempDir("7z"))
+            {
+                var temp7ZExe = temp.Root.Add("7za.exe", DetectOptions.SetToFile);
+                Extract(temp7ZExe.FullName, CompressionResources._7za);
+                var temp7ZipDll = temp.Root.Add("7za.dll", DetectOptions.SetToFile);
+                Extract(temp7ZipDll.FullName, CompressionResources._7za1);
+                var temp7Zip32Dll = temp.Root.Add("7zxa.dll", DetectOptions.SetToFile);
+                Extract(temp7Zip32Dll.FullName, CompressionResources._7zxa);
+
+                var paramsString = @params.Aggregate(string.Empty, (current, param) => current + $" {param}");
+                var script = $"{temp7ZExe.FullName} {command} {paramsString} -y";
+                return CommandPrompt.Run(script, _logger);
+            }
+        }
+
+        private void Extract(string path, byte[] bytes)
+        {
+            using (var fsDst = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            {
+                fsDst.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
