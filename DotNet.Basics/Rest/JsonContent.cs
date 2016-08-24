cdbc389f3a273b43a83ab47e0125e61b9d3ecd29
rzmoz <rzmoz@users.noreply.github.com>
2016-08-24 15:14:32 +0800

    Added json content as first class citizen in rest ns

---
+++ DotNet.Basics/Rest/JsonContent.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace DotNet.Basics.Rest
+{
+    public class JsonContent : StringContent
+    {
+        private static readonly JsonRestSerializer _serializer = new JsonRestSerializer();
+
+        public const string ContentType = "application/json";
+
+        public JsonContent(string json) : base(json, Encoding.UTF8, ContentType)
+        {
+        }
+        public JsonContent(string json, Encoding encoding) : base(json, encoding, ContentType)
+        { }
+
+        public JsonContent(object content) : base(SerializeContent(content), Encoding.UTF8, ContentType)
+        {
+        }
+
+        public JsonContent(object content, Encoding encoding) : base(SerializeContent(content), encoding, ContentType)
+        {
+
+        }
+
+        private static string SerializeContent(object content)
+        {
+            if (content == null)
+                return "{}";
+
+            return _serializer.Serialize(content);
+        }
+    }
+}
