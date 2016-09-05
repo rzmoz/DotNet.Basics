a573d92dc28edb7c9897a45745f20be6d6e573a3
rzmoz <rzmoz@users.noreply.github.com>
2016-09-05 18:16:00 +0800

    add runinbg runner for easier access with support for run as singleton

---
+++ DotNet.Basics.Tests/Tasks/BackgroundTaskRunnerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DotNet.Basics.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.Tasks
+{
+    [TestFixture]
+    public class BackgroundTaskRunnerTests
+    {
+        [Test]
+        public async Task RunSync_GetExceptionsFromBgTasks_ExceptionsAreBubbled()
+        {
+            var taskId = "RunSync_GetExceptionsFromBgTasks_ExceptionsAreBubbled";
+
+            await AssertTaskAsync(taskId, bgRunner =>
+            {
+                bgRunner.Start(() =>
+                {
+                    throw new ArgumentNullException();
+
+                }, taskId, true);
+            }).ConfigureAwait(false);
+        }
+        [Test]
+        public async Task RunAsync_GetExceptionsFromBgTasks_ExceptionsAreBubbled()
+        {
+            var taskId = "RunAsync_GetExceptionsFromBgTasks_ExceptionsAreBubbled";
+
+            await AssertTaskAsync(taskId, bgRunner =>
+             {
+                 bgRunner.Start(ct =>
+                 {
+                     throw new ArgumentNullException();
+
+                 }, taskId, true);
+             }).ConfigureAwait(false);
+        }
+
+        private async Task AssertTaskAsync(string taskId, Action<BackgroundTaskRunner> startTaskCallback)
+        {
+            bool taskRan = false;
+            Exception lastException = null;
+            string lastTaskId = null;
+            string lastRunId = null;
+
+            bool taskEnded = false;
+
+            var bgRunner = new BackgroundTaskRunner();
+            bgRunner.Starting += (tid, rid, started, reason) =>
+            {
+                taskRan = true;
+                lastTaskId = tid;
+                lastRunId = rid;
+            };
+            bgRunner.Ended += (tid, rid, e) =>
+            {
+                lastException = e;
+                taskEnded = true;
+            };
+
+            startTaskCallback(bgRunner);
+
+            while (taskEnded == false)
+                await Task.Delay(50.Milliseconds(), CancellationToken.None).ConfigureAwait(false);
+
+            taskRan.Should().BeTrue();
+            lastTaskId.Should().Be(taskId);
+            lastRunId.Should().NotBeNull();
+            lastException.Should().BeOfType<ArgumentNullException>();
+        }
+    }
+}
---
+++ DotNet.Basics/Tasks/BackgroundTaskRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet.Basics.Tasks
+{
+    public class BackgroundTaskRunner
+    {
+        public event ManagedTask.TaskStartingEventHandler Starting;
+        public event ManagedTask.TaskEndedEventHandler Ended;
+
+        public void Start(Action task, string id = null, bool asSingleton = false)
+        {
+            Start(new ManagedTask(task, id), asSingleton);
+        }
+        public void Start(Func<CancellationToken, Task> task, string id = null, bool asSingleton = false)
+        {
+            Start(new ManagedTask(task, id), asSingleton);
+        }
+
+        private void Start(ManagedTask task, bool asSingleton)
+        {
+            if (asSingleton)
+                task = new SingletonTask(task.Run, task.RunAsync, task.Id);
+            task = new BackgroundTask(task.Run, task.RunAsync, task.Id);
+            task.Starting += (tid, rid, started, reason) => { Starting?.Invoke(tid, rid, started, reason); };
+            task.Ended += (tid, rid, e) => { Ended?.Invoke(tid, rid, e); };
+            task.Run();
+        }
+    }
+}
