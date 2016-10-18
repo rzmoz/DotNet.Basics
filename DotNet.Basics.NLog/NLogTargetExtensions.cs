632a0ef9ccd7228bbb662db38287b927aca21b85
rzmoz <rzmoz@users.noreply.github.com>
2016-10-18 12:04:24 +0800

    Added proj for simpler and more fluent nlog configuration

---
+++ DotNet.Basics.NLog/NLogTargetExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NLog.Targets;
+using NLog.Targets.Wrappers;
+
+namespace DotNet.Basics.NLog
+{
+    public static class NLogTargetExtensions
+    {
+        public static BufferingTargetWrapper AsBuffered(this Target wrappedTarget, int bufferSize = 100,
+            int flushTimeout = 1000)
+        {
+            return new BufferingTargetWrapper(wrappedTarget, bufferSize, flushTimeout);
+        }
+
+        public static AsyncTargetWrapper AsAsync(this Target wrappedTarget, int queueLimit = 10000, AsyncTargetWrapperOverflowAction overflowAction = AsyncTargetWrapperOverflowAction.Grow)
+        {
+            return new AsyncTargetWrapper(wrappedTarget, queueLimit, overflowAction);
+        }
+    }
+}
