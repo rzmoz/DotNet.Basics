34761d97acc9f7d96a00263e67a8e56ce0e83073
rzmoz <rzmoz@users.noreply.github.com>
2016-10-22 18:04:33 +0800

    added handling of windows services (start / stop)

---
+++ DotNet.Basics.Tests/Sys/WindowsServicesTests.cs
@@ -0,0 +1,40 @@
+﻿using DotNet.Basics.Sys;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNet.Basics.Tests.Sys
+{
+    [TestFixture]
+    public class WindowsServicesTests
+    {
+        [Test]
+        public void Exists_Exists_ServiceExists()
+        {
+            var serviceName = "Spooler";
+
+            var exists = WindowsServices.Exists(serviceName);
+
+            exists.Should().BeTrue($"Service exists: {serviceName}");
+        }
+
+        [Test]
+        public void Exists_Exists_ServiceDoesNotExist()
+        {
+            var serviceName = "Exists_Exists_ServiceDoesNotExist";
+
+            var exists = WindowsServices.Exists(serviceName);
+
+            exists.Should().BeFalse($"Service exists: {serviceName}");
+        }
+
+        [Test]
+        public void Get_Status_StatusFromServicesIsRetrieved()
+        {
+            var serviceName = "Spooler";
+
+            var isrunning = WindowsServices.IsRunning(serviceName);
+
+            isrunning.Should().BeTrue(serviceName);
+        }
+    }
+}
---
+++ DotNet.Basics/Sys/WindowsServices.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.ServiceProcess;
+using DotNet.Basics.Tasks.Repeating;
+
+namespace DotNet.Basics.Sys
+{
+    public static class WindowsServices
+    {
+        public static bool Exists(string serviceName)
+        {
+            if (serviceName == null) throw new ArgumentNullException(nameof(serviceName));
+            return
+                ServiceController.GetServices()
+                    .Any(ctrl => ctrl.ServiceName.Equals(serviceName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static bool IsRunning(string serviceName)
+        {
+            using (var ctrl = Get(serviceName))
+            {
+                return ctrl?.Status == ServiceControllerStatus.Running;
+            }
+        }
+
+        public static bool Start(string serviceName)
+        {
+            return InvokeService(serviceName, service => service.Start(), ServiceControllerStatus.Running);
+        }
+
+        public static bool Stop(string serviceName)
+        {
+            return InvokeService(serviceName, service => service.Stop(), ServiceControllerStatus.Stopped);
+        }
+
+        public static bool Restart(string serviceName)
+        {
+            return Stop(serviceName) && Start(serviceName);
+        }
+
+        private static bool InvokeService(string serviceName, Action<ServiceController> serviceAction, ServiceControllerStatus exitStatus)
+        {
+            if (serviceName == null) throw new ArgumentNullException(nameof(serviceName));
+            using (var service = Get(serviceName))
+            {
+                if (service == null)
+                    return false;
+
+                var success = Repeat.TaskOnce(() => serviceAction(service))
+                    .WithOptions(o =>
+                    {
+                        o.RetryDelay = 500.MilliSeconds();
+                        o.Timeout = 10.Seconds();
+                    }).Until(() => service.Status == exitStatus);
+                return success;
+            }
+        }
+
+        private static ServiceController Get(string serviceName)
+        {
+            if (serviceName == null) throw new ArgumentNullException(nameof(serviceName));
+            var service = ServiceController.GetServices().FirstOrDefault(ctrl => ctrl.ServiceName.Equals(serviceName, StringComparison.OrdinalIgnoreCase));
+            if (service == null)
+                DebugOut.WriteLine($"WindowsService not found: {serviceName}");
+            return service;
+        }
+    }
+}
