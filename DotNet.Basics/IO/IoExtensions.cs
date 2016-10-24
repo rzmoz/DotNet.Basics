4f093be2c815d16a7e228ed83c47ede4e18bd330
rzmoz <rzmoz@users.noreply.github.com>
2016-10-24 14:15:20 +0800

    fixed so write all text doesnt fail silently if target file already exists

--- DotNet.Basics/IO/IoExtensions.cs
+++ DotNet.Basics/IO/IoExtensions.cs
@@ -5,21 +5,22 @@ namespace DotNet.Basics.IO
 {
     public static class IoExtensions
     {
-        public static bool WriteAllText(this string content, FilePath targetFile, bool overwrite = false)
+        public static void WriteAllText(this string content, FilePath targetFile, bool overwrite = true)
         {
             if (targetFile == null) throw new ArgumentNullException(nameof(targetFile));
-
             if (overwrite == false && targetFile.Exists())
-                return false;
+                throw new IOException($"Cannot write text. Target file already exists. Set overwrite to true to ignore existing file: {targetFile}");
 
             targetFile.Directory.CreateIfNotExists();
             File.WriteAllText(targetFile.FullName, content ?? string.Empty);
-            return true;
         }
 
-        public static bool WriteAllText(this string content, PathInfo target, bool overwrite = false)
+        public static void WriteAllText(this string content, PathInfo target, bool overwrite = true)
         {
-            return !target.IsFolder && content.WriteAllText(target as FilePath, overwrite);
+            if (target.IsFolder)
+                throw new ArgumentException($"Cannot write text. Target is a folder: {target}");
+
+            content.WriteAllText(target as FilePath, overwrite);
         }
     }
 }
