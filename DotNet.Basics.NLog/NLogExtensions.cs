3d543869519006fee36e0188f26f4cb7a4805227
rzmoz <rzmoz@users.noreply.github.com>
2016-11-12 17:49:56 +0800

    Added log extensions for easier inline debugging - no need to instantiate iloggers everywhere

---
+++ DotNet.Basics.NLog/NLogExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+
+namespace DotNet.Basics.NLog
+{
+    public static class NLogExtensions
+    {
+        public static Logger NLog<T>(this object @obj)
+        {
+            return @obj.NLog(typeof(T).FullName);
+        }
+        public static Logger NLog(this object @obj)
+        {
+            return @obj.NLog(@obj.GetType().FullName);
+        }
+        public static Logger NLog(this object @obj, string loggerName)
+        {
+            return LogManager.GetLogger(loggerName);
+        }
+    }
+}
