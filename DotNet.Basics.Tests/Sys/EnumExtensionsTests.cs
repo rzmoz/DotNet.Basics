ac1ea458c976a5d63a9fca2bb01e5a899a7c825a
rzmoz <rzmoz@users.noreply.github.com>
2017-11-10 14:19:31 +0100

    added enum extension helpers

---
+++ DotNet.Basics.Tests/Sys/EnumExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNet.Basics.Sys;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNet.Basics.Tests.Sys
+{
+    public class EnumExtensionsTests
+    {
+        [Fact]
+        public void ToName_ParseRightType_NameIsPArsedNotNumValue()
+        {
+            var result = TestEnum.This.ToName();
+
+            result.Should().Be("This");
+        }
+        
+        [Fact]
+        public void ToEnum_Parse_EnumIsParsed()
+        {
+            var @enum = "This".ToEnum<TestEnum>();
+            @enum.Should().Be(TestEnum.This);
+        }
+
+        [Fact]
+        public void ToEnum_InvalidInput_ExceptionIsThrown()
+        {
+            Action act = () => "SomethingNotValidxxxxxxxx".ToEnum<TestEnum>();
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void ToEnum_ParseCaseInsensitive_EnumIsParsed()
+        {
+            var @enum = "tHis".ToEnum<TestEnum>();
+            @enum.Should().Be(TestEnum.This);
+        }
+
+        [Fact]
+        public void ToEnum_HasMultiple_EnumHasAll()
+        {
+            const TestEnum value = TestEnum.This | TestEnum.That;
+
+            value.Has(TestEnum.This).Should().BeTrue();
+            value.Has(TestEnum.That).Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsEnum_Parse_True()
+        {
+            var result = "This".IsEnum<TestEnum>();
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsEnum_InvalidInput_False()
+        {
+            var result = "SomethingNotValidxxxxxxxx".IsEnum<TestEnum>();
+            result.Should().BeFalse();
+        }
+        
+
+        private enum TestEnum
+        {
+            This = 1,
+            That = 2,
+            More = 4,
+            EvenMore = 8,
+            EvenMoreMore = 16
+        }
+    }
+}
---
+++ DotNet.Basics/Sys/EnumExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace DotNet.Basics.Sys
+{
+    public static class EnumExtensions
+    {
+        public static T ToEnum<T>(this string enumValue, bool ignoreCase = true) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new NotSupportedException();
+            return (T)Enum.Parse(typeof(T), enumValue, ignoreCase);
+        }
+
+        public static bool IsEnum<T>(this string enumValue) where T : struct
+        {
+            try
+            {
+                enumValue.ToEnum<T>();
+                return true;
+            }
+            catch (NotSupportedException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        public static bool Has<T>(this Enum type, T value)
+        {
+            try { return (((int)(object)type & (int)(object)value) == (int)(object)value); }
+            catch { return false; }
+        }
+
+        public static string ToName(this Enum @enum)
+        {
+            return Enum.GetName(@enum.GetType(), @enum);
+        }
+    }
+}
