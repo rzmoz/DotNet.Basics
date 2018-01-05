c5b1110653ca63287f81270274cdddcea8818160
rzmoz <rzmoz@users.noreply.github.com>
2018-01-05 12:20:56 +0100

    fixed nlog bootstrapping for IServiceCollection (dotnet core)

--- DotNet.Standard.Extensions.Logging/NLogExtensions.cs
+++ DotNet.Standard.Extensions.Logging/NLogExtensions.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Config;
+using NLog.Extensions.Logging;
+using NLog.Targets;
 
 namespace DotNet.Standard.Extensions.Logging
 {
     public static class NLogExtensions
     {
-        public static void AddNLogging(this IServiceCollection services, LogLevel logLevel = LogLevel.Trace)
+        public static void AddNLogging(this IServiceCollection services, Action<NLogConfigurator> addTargets = null, LoggingConfiguration config = null, LogLevel minimumLogLevel = LogLevel.Trace)
         {
+            using (var conf = new NLogConfigurator(config))
+            {
+                if (addTargets == null)
+                    conf.AddTarget(new ColoredConsoleTarget().WithOutputColors());
+                else
+                    addTargets(conf);
+            }
+
             services.AddSingleton<ILoggerFactory, LoggerFactory>();
-            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
-            services.AddLogging(builder => builder.SetMinimumLevel(logLevel));
+            services.AddLogging(builder => builder.AddNLog().SetMinimumLevel(minimumLogLevel));
         }
     }
 }
