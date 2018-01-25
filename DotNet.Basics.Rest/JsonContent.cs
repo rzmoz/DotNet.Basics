f3ee7bac4cedf2094cd15c7d180916ad6bbb8e8d
rzmoz <rzmoz@users.noreply.github.com>
2018-01-25 14:32:45 +0100

    Fixed so JsonContent can consume objects instead of just strings

--- DotNet.Basics.Rest/JsonContent.cs
+++ DotNet.Basics.Rest/JsonContent.cs
@@ -1,16 +1,24 @@
 ﻿using System.Net.Http;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace DotNet.Basics.Rest
 {
     public class JsonContent : StringContent
     {
-        private const string _defaultContentType = "application/json";
+        public const string DefaultContentType = "application/json";
+        public static readonly Encoding DefaultEncoding = Encoding.UTF8;
 
-        public JsonContent(string json) : this(json, Encoding.UTF8)
+        public JsonContent(object obj) : this(JsonConvert.SerializeObject(obj), DefaultEncoding)
         { }
 
-        public JsonContent(string json, Encoding encoding) : base(json, encoding, _defaultContentType)
+        public JsonContent(object obj, Encoding encoding) : this(JsonConvert.SerializeObject(obj), encoding)
+        { }
+
+        public JsonContent(string json) : this(json, DefaultEncoding)
+        { }
+
+        public JsonContent(string json, Encoding encoding) : base(json, encoding, DefaultContentType)
         { }
     }
 }
