f738821ef4cafa21e4d4c16fc867c9869a52f50a
rzmoz <rzmoz@users.noreply.github.com>
2018-07-26 16:23:44 +0200

    Added rest client with events for easier debugging

---
+++ DotNet.Basics.Rest/RestClientWithEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNet.Basics.Rest
+{
+    public class RestClientWithEvents : RestClient
+    {
+        public delegate void RequestHandler(HttpRequestMessage request);
+        public delegate void ResponseHandler(HttpResponseMessage response);
+
+        public event RequestHandler RequestSending;
+        public event ResponseHandler ResponseReceived;
+
+        public RestClientWithEvents(string baseUri = null, HttpClient httpClient = null) : base(baseUri, httpClient)
+        { }
+
+        public override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            RequestSending?.Invoke(request);
+            var response = await base.SendAsync(request).ConfigureAwait(false);
+            ResponseReceived?.Invoke(response);
+            return response;
+        }
+    }
+}
---
+++ DotNet.Basics.Tests/Rest/RestClientWithEventsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using DotNet.Basics.Rest;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNet.Basics.Tests.Rest
+{
+    public class RestClientWithEventsTests
+    {
+        [Fact]
+        public async Task RequestSending_Events_EventIsRaised()
+        {
+            //arrange
+            var client = new RestClientWithEvents("https://cdnjs.cloudflare.com/ajax/libs/mogl/0.3.0/");
+
+            HttpRequestMessage receivedRequest = null;
+            HttpResponseMessage receivedResponse = null;
+
+            client.RequestSending += req => receivedRequest = req;
+            client.ResponseReceived += res => receivedResponse = res;
+
+            //act
+            var response = await client.GetAsync("mogl.min.js").ConfigureAwait(false);
+
+            //assert
+            receivedRequest.Should().NotBeNull();
+            receivedResponse.Should().NotBeNull();
+            receivedRequest.RequestUri.ToString().Should().Be("https://cdnjs.cloudflare.com/ajax/libs/mogl/0.3.0/mogl.min.js");
+            receivedResponse.RequestMessage.RequestUri.ToString().Should().Be("https://cdnjs.cloudflare.com/ajax/libs/mogl/0.3.0/mogl.min.js");
+        }
+    }
+}
