f4b70202c57083bc1af2059f56022f0b6f1dc17c
rzmoz <rzmoz@users.noreply.github.com>
2018-10-31 10:29:09 +0100

    removed unused code

--- DotNet.Basics/Collections/IMultiLevelDictionary.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DotNet.Basics.Collections
-{
-    public interface IMultiLevelDictionary<T> : IDictionary<string, T>
-    {
-        char PathSeparator { get; }
-    }
-}
