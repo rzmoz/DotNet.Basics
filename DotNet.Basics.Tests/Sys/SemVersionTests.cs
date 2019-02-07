e7002ab738000672cc9709ced170c816167c2900
rzmoz <rzmoz@users.noreply.github.com>
2019-02-07 10:18:15 +0100

    Added initial semver classes

---
+++ DotNet.Basics.Tests/Sys/SemVersionTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNet.Basics.Sys;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNet.Basics.Tests.Sys
+{
+    public class SemVersionTests
+    {
+        [Fact]
+        public void PreRelease_Precedence_PreReleaseIsIgnoredWhenAnyMajorMinorOrPatchIsDifferent()
+        {
+            var preRelease = "rc.1";
+            var higherWithPreRelease = new SemVersion(1, 0, 2, preRelease);
+            var lowerWithPreRelease = new SemVersion(1, 0, 0, preRelease);
+
+            //smaller than
+            (lowerWithPreRelease < higherWithPreRelease).Should().BeTrue();
+
+            //larger than
+            (lowerWithPreRelease > higherWithPreRelease).Should().BeFalse();
+        }
+
+        [Fact]
+        public void PreRelease_Precedence_AnyPreReleaseTakesLowerPrecedence()
+        {
+            var woPreRelease = new SemVersion(1, 0, 0);
+            var withPreRelease = new SemVersion(1, 0, 0, "rc.1");
+
+            //smaller than
+            (withPreRelease < woPreRelease).Should().BeTrue();
+
+            //larger than
+            (withPreRelease > woPreRelease).Should().BeFalse();
+        }
+
+        [Fact]
+        public void MajorMinorPatch_Precedence_HigherVersionTakesPrecedence()
+        {
+            var order1 = new SemVersion(1, 0, 0);
+            var order2 = new SemVersion(2, 0, 0);
+            var order3 = new SemVersion(2, 1, 0);
+            var order4 = new SemVersion(2, 1, 1);
+
+            //smaller than
+            (order1 < order2).Should().BeTrue();
+            (order1 < order3).Should().BeTrue();
+            (order1 < order4).Should().BeTrue();
+            (order2 < order3).Should().BeTrue();
+            (order2 < order4).Should().BeTrue();
+            (order3 < order4).Should().BeTrue();
+
+            //larger than
+            (order1 > order2).Should().BeFalse();
+            (order1 > order3).Should().BeFalse();
+            (order1 > order4).Should().BeFalse();
+            (order2 > order3).Should().BeFalse();
+            (order2 > order4).Should().BeFalse();
+            (order3 > order4).Should().BeFalse();
+        }
+    }
+}
---
+++ DotNet.Basics/Sys/SemVersion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DotNet.Basics.Sys
+{
+    public class SemVersion
+    {
+        public SemVersion(string semVerString)
+        {
+            var semVer = Parse(semVerString);
+            Major = semVer.Major;
+            Minor = semVer.Minor;
+            Patch = semVer.Patch;
+            PreRelease = semVer.PreRelease;
+            Metadata = semVer.Metadata;
+        }
+
+        public SemVersion(uint major, uint minor, uint patch, string preRelease = null, string metadata = null)
+        {
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+            PreRelease = preRelease ?? string.Empty;
+            Metadata = metadata ?? string.Empty;
+        }
+
+        public uint Major { get; }
+        public uint Minor { get; }
+        public uint Patch { get; }
+        public string PreRelease { get; }
+        public string Metadata { get; }
+
+        public static SemVersion Parse(string semVer)
+        {
+            return new SemVersion(0, 0, 0);
+        }
+
+        public static bool operator <(SemVersion a, SemVersion b)
+        {
+            if (a.Major < b.Major)
+                return true;
+            if (a.Minor < b.Minor)
+                return true;
+            if (a.Patch < b.Patch)
+                return true;
+            return false;
+        }
+        public static bool operator >(SemVersion a, SemVersion b)
+        {
+            if (a.Major > b.Major)
+                return true;
+            if (a.Minor > b.Minor)
+                return true;
+            if (a.Patch > b.Patch)
+                return true;
+            return false;
+        }
+    }
+}
