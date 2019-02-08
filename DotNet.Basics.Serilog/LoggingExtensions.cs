54077618429f65eb572d0b28085fa4c7bd470af6
rzmoz <rzmoz@users.noreply.github.com>
2019-02-08 11:54:44 +0100

    Added converter from ms loglevel to serilog log event level

---
+++ DotNet.Basics.Serilog/LoggingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+
+namespace DotNet.Basics.Serilog
+{
+    public static class LoggingExtensions
+    {
+        public static LogEventLevel ToSeriLogEventLevel(this LogLevel lvl)
+        {
+            switch (lvl)
+            {
+                case LogLevel.None:
+                case LogLevel.Trace:
+                    return LogEventLevel.Verbose;
+                case LogLevel.Debug:
+                    return LogEventLevel.Debug;
+                case LogLevel.Information:
+                    return LogEventLevel.Information;
+                case LogLevel.Warning:
+                    return LogEventLevel.Warning;
+                case LogLevel.Error:
+                    return LogEventLevel.Error;
+                case LogLevel.Critical:
+                    return LogEventLevel.Fatal;
+                default:
+                    throw new ArgumentException($"Level unknown: {lvl}");
+            }
+        }
+    }
+}
---
+++ DotNet.Basics.Tests/Serilog/LoggingExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNet.Basics.Serilog;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+using Xunit;
+
+namespace DotNet.Basics.Tests.Serilog
+{
+    public class LoggingExtensionsTests
+    {
+        [Fact]
+        public void ToSeriLogEventLevel_Conversion_LevelIsConvertedFromMicrosoftExtensionLogLevel()
+        {
+            LogLevel.None.ToSeriLogEventLevel().Should().Be(LogEventLevel.Verbose);
+            LogLevel.Trace.ToSeriLogEventLevel().Should().Be(LogEventLevel.Verbose);
+            LogLevel.Debug.ToSeriLogEventLevel().Should().Be(LogEventLevel.Debug);
+            LogLevel.Information.ToSeriLogEventLevel().Should().Be(LogEventLevel.Information);
+            LogLevel.Warning.ToSeriLogEventLevel().Should().Be(LogEventLevel.Warning);
+            LogLevel.Error.ToSeriLogEventLevel().Should().Be(LogEventLevel.Error);
+            LogLevel.Critical.ToSeriLogEventLevel().Should().Be(LogEventLevel.Fatal);
+        }
+    }
+}
