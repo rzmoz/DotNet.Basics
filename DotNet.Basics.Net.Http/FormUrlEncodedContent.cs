5103c2139bb4f4153d4d9bdcd773b6eaec41bdd2
rzmoz <rzmoz@users.noreply.github.com>
2019-03-08 13:38:33 +0100

    Made ToQueryString from key value pairs public

--- DotNet.Basics.Net.Http/FormUrlEncodedContent.cs
+++ DotNet.Basics.Net.Http/FormUrlEncodedContent.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-using System.Web;
 
 namespace DotNet.Basics.Net.Http
 {
@@ -9,17 +8,9 @@ namespace DotNet.Basics.Net.Http
     {
         public const string DefaultMediaType = "application/x-www-form-urlencoded";
 
-        public FormUrlEncodedContent(IEnumerable<KeyValuePair<string, string>> content, Encoding encoding=null)
-            : base(ParseContent(content), encoding, DefaultMediaType)
+        public FormUrlEncodedContent(IEnumerable<KeyValuePair<string, string>> content, Encoding encoding = null)
+            : base(content.ToQueryString(), encoding, DefaultMediaType)
         {
-        }
-
-        private static string ParseContent(IEnumerable<KeyValuePair<string, string>> content)
-        {
-            var bodyBuilder = new StringBuilder();
-            foreach (var entry in content)
-                bodyBuilder.Append($"&{entry.Key}={HttpUtility.UrlEncode(entry.Value ?? string.Empty)}");
-            return bodyBuilder.ToString().TrimStart('&');
         }
     }
 }
--- DotNet.Basics.Net.Http/HttpContentExtensions.cs
+++ DotNet.Basics.Net.Http/HttpContentExtensions.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Newtonsoft.Json;
 
 namespace DotNet.Basics.Net.Http
@@ -8,6 +11,14 @@ namespace DotNet.Basics.Net.Http
     public static class HttpContentExtensions
     {
         private const char _stringQuote = '\"';
+
+        public static string ToQueryString(this IEnumerable<KeyValuePair<string, string>> keyValues)
+        {
+            var bodyBuilder = new StringBuilder();
+            foreach (var entry in keyValues)
+                bodyBuilder.Append($"&{entry.Key}={HttpUtility.UrlEncode(entry.Value ?? string.Empty)}");
+            return bodyBuilder.ToString().TrimStart('&');
+        }
 
         public static async Task<T> ReadAsTypeAsync<T>(this HttpContent content, JsonSerializerSettings jsonSerializerSettings = null)
         {
