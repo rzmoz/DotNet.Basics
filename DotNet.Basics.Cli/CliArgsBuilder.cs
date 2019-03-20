3354fbf8bc1b418f85976326f9b40a3503052faa
rzmoz <rzmoz@users.noreply.github.com>
2019-03-20 12:23:19 +0100

    Added easy init of serilog in cli

--- DotNet.Basics.Cli/CliArgsBuilder.cs
+++ DotNet.Basics.Cli/CliArgsBuilder.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace DotNet.Basics.Cli
 {
     public class CliArgsBuilder
     {
         private const string _microsoftExtensionsArgsSwitch = "--";
+
+        public CliArgsBuilder WithSerilog(Action<LoggerConfiguration> configureLogger = null)
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.ColoredConsole();
+
+            configureLogger?.Invoke(loggerConfiguration);
+            Log.Logger = loggerConfiguration.CreateLogger();
+            return this;
+        }
 
         public CliArgs Build(string[] args, Action<IConfigurationBuilder> add = null, string argsSwitch = "-")
         {
