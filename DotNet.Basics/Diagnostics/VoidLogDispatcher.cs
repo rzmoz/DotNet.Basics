948c3ac595cc6214861e66f9e75c6245a12f5502
rzmoz <rzmoz@users.noreply.github.com>
2019-04-04 15:46:28 +0200

    cc

--- DotNet.Basics/Diagnostics/VoidLogDispatcher.cs
+++ DotNet.Basics/Diagnostics/VoidLogDispatcher.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 
 namespace DotNet.Basics.Diagnostics
 {
-    public class NullLogDispatcher : ILogDispatcher
+    public class VoidLogDispatcher : ILogDispatcher
     {
         public event LogDispatcher.MessageLoggedEventHandler MessageLogged;
         public ILogDispatcher InContext(string context, bool floatMessageLogged = true)
