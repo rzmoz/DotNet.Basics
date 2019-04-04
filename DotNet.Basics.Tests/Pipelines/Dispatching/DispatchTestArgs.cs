115ebc6e53026af9f129e63e69a54b47f306fe67
rzmoz <rzmoz@users.noreply.github.com>
2019-04-04 22:46:05 +0200

    Set most libs to release version

--- DotNet.Basics.Tests/Pipelines/Dispatching/DispatchTestArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNet.Basics.Tests.Pipelines.Dispatching
-{
-    public class DispatchTestArgs
-    {
-        public string SetByArgs { get; set; }
-        public string SetByPipeline { get; set; }
-        public string[] SplitArgs { get; set; }
-    }
-}
