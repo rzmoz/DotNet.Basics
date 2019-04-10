d902f75e56d634af8838bca1f88db4eaf90ec5b9
rzmoz <rzmoz@users.noreply.github.com>
2019-04-10 12:04:04 +0200

    fixed pause for debug to be exact match on flag - was too greedy before

--- DotNet.Basics.Cli/ArgsExtensions.cs
+++ DotNet.Basics.Cli/ArgsExtensions.cs
@@ -7,34 +7,46 @@ namespace DotNet.Basics.Cli
     public static class ArgsExtensions
     {
         public static readonly char[] ConfigurationSwitchFlags = { '-', '/' };
-        
+
         public static bool IsSet(this string[] args, string key, bool firstCharIsShortKey = true, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
         {
             if (key == null) throw new ArgumentNullException(nameof(key));
+            if (key.Length < 0)
+                throw new ArgumentException($"{nameof(key)} is empty string");
 
-            var strippedKey = key.TrimStart(ConfigurationSwitchFlags);
-
-            //full match
-            if (args.Any(a => a.TrimStart(ConfigurationSwitchFlags).Equals(strippedKey, stringComparison)))
+            if (args.IsSet(key, stringComparison))
                 return true;
+            if (firstCharIsShortKey)
+                return args.IsSet(key[0].ToString(), stringComparison);
+            return false;
+        }
 
-            if (firstCharIsShortKey == false)
-                return false;
-
-            var shortKey = char.ToLowerInvariant(strippedKey[0]);
+        private static bool IsSet(this string[] args, string key, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (key.Length < 0)
+                throw new ArgumentException($"{nameof(key)} is empty string");
 
             return args.Any(a =>
             {
-                var loweredArg = char.ToLowerInvariant(a.TrimStart(ConfigurationSwitchFlags)[0]);
-                return loweredArg == shortKey;
+                foreach (var configurationSwitchFlag in ConfigurationSwitchFlags)
+                {
+                    if (a.Equals($"{configurationSwitchFlag}{key}", stringComparison))
+                        return true;
+                }
+
+                return false;
             });
         }
 
         public static void PauseIfDebug(this string[] args, string debugFlag = "debug")
         {
-            if (args.IsSet(debugFlag))
+            if (string.IsNullOrWhiteSpace(debugFlag) || debugFlag.Length < 1)
+                return;
+
+            if (args.IsSet(debugFlag, firstCharIsShortKey: true))
             {
-                Console.WriteLine($"Paused for debug. Name: {Process.GetCurrentProcess().Id} | PID: {Process.GetCurrentProcess().ProcessName}. Press [ENTER] to continue..");
+                Console.WriteLine($"Paused for debug. Name: {Process.GetCurrentProcess().ProcessName} | PID: {Process.GetCurrentProcess().Id}. Press [ENTER] to continue..");
                 Console.ReadLine();
             }
         }
