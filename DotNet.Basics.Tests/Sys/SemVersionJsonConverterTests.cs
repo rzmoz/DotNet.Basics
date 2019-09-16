a68a938f2998847553a0243e126e5c709eafd597
rzmoz <rzmoz@users.noreply.github.com>
2019-09-16 17:12:14 +0200

    add system.text.jsonconverter to semversion

---
+++ DotNet.Basics.Tests/Sys/SemVersionJsonConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using DotNet.Basics.Sys;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNet.Basics.Tests.Sys
+{
+    public class SemVersionJsonConverterTests
+    {
+        [Fact]
+        public void Convert_Serialize_PathIsSerialized()
+        {
+            var rawVersion = @"1.0.3-beta.1.2+HelloWorld";
+            var semVer = new SemVersion(rawVersion);
+
+            var json = JsonSerializer.Serialize(semVer, new JsonSerializerOptions
+            {
+                Converters = { new SemVersionJsonConverter() }
+            });
+
+            json.Should().Be($@"""{rawVersion.Replace("\\", "\\\\")}""");
+        }
+
+        [Fact]
+        public void Convert_Deserialize_PathIsDeserialized()
+        {
+            var rawVersion = @"1.0.3-beta.1.2+HelloWorld";
+            var version = JsonSerializer.Deserialize<SemVersion>($@"""{rawVersion.Replace("\\", "\\\\")}""", new JsonSerializerOptions
+            {
+                Converters = { new SemVersionJsonConverter() }
+            });
+
+            version.SemVer20String.Should().Be(rawVersion);
+        }
+    }
+}
---
+++ DotNet.Basics/Sys/SemVersionJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNet.Basics.Sys
+{
+    public class SemVersionJsonConverter : JsonConverter<SemVersion>
+    {
+        public override SemVersion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return new SemVersion(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, SemVersion value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.SemVer20String);
+        }
+    }
+}
