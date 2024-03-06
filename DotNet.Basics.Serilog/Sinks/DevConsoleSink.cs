d21dfc2f7fa1dfb106b376a49d877d7ac7a4ce72
rzmoz <rzmoz@users.noreply.github.com>
2024-03-06 10:39:31 +0100

    Added colors to devconsole output

--- DotNet.Basics.Serilog/Sinks/DevConsoleSink.cs
+++ DotNet.Basics.Serilog/Sinks/DevConsoleSink.cs
@@ -1,20 +1,19 @@
-﻿using System;
+﻿
+
+using System;
 using Serilog.Core;
 using Serilog.Events;
 
 namespace DotNet.Basics.Serilog.Sinks
 {
-    public class DevConsoleSink : ILogEventSink
+    public class DevConsoleSink(bool isADO) : ILogEventSink
     {
-        private readonly Func<LogEvent, string> _renderMessage = le => le.RenderMessage();
+        private static readonly object _messageLock = new();
+        private readonly Func<LogEvent, string> _renderMessage = le => isADO ? RenderAdoMessage(le) : le.RenderMessage();
+        private readonly Func<LogEventLevel, ConsoleColor> _getConsoleColor = le => isADO ? ConsoleColor.White : GetColor(le);
 
-        public DevConsoleSink(bool isADO)
-        {
-            if (isADO)
-                _renderMessage = RenderAdoMessage;
-        }
 
-        private string RenderAdoMessage(LogEvent logEvent)
+        private static string RenderAdoMessage(LogEvent logEvent)
         {
             switch (logEvent.Level)
             {
@@ -37,10 +36,36 @@ namespace DotNet.Basics.Serilog.Sinks
         {
             var msg = _renderMessage(logEvent);
 
-            if (logEvent.Level < LogEventLevel.Error)
-                Console.WriteLine(msg);
-            else
-                Console.Error.WriteLine(msg);
+            lock (_messageLock)
+            {
+                Console.ForegroundColor = _getConsoleColor(logEvent.Level);
+                if (logEvent.Level < LogEventLevel.Error)
+                    Console.WriteLine(msg);
+                else
+                    Console.Error.WriteLine(msg);
+                Console.ResetColor();
+            }
+        }
+
+        private static ConsoleColor GetColor(LogEventLevel lvl)
+        {
+            switch (lvl)
+            {
+                case LogEventLevel.Verbose:
+                    return ConsoleColor.DarkGray;
+                case LogEventLevel.Debug:
+                    return ConsoleColor.Gray;
+                case LogEventLevel.Information:
+                    return ConsoleColor.Cyan;
+                case LogEventLevel.Warning:
+                    return ConsoleColor.Yellow;
+                case LogEventLevel.Error:
+                    return ConsoleColor.Red;
+                case LogEventLevel.Fatal:
+                    return ConsoleColor.DarkRed;
+                default:
+                    return ConsoleColor.White;
+            }
         }
     }
 }
