bde63ddc45a4338e688a5e38cb459e989dcda127
rzmoz <rzmoz@users.noreply.github.com>
2025-02-20 14:38:30 +0100

    Added events to cmd prompt logger for easier chaining of logging to other frameworks

--- DotNet.Basics/Win/CmdPrompt.cs
+++ DotNet.Basics/Win/CmdPrompt.cs
@@ -1,6 +1,5 @@
 ﻿using DotNet.Basics.Sys;
 using System;
-using System.Text;
 
 namespace DotNet.Basics.Win
 {
@@ -15,31 +14,6 @@ namespace DotNet.Basics.Win
         public static int Run(string commandString, Action<string> writeOutput, Action<string> writeError, Action<string> writeDebug)
         {
             return ExternalProcess.Run("cmd.exe", $"/c {commandString}", writeOutput, writeError, writeDebug);
-        }
-    }
-
-    public class CmdPromptLogger
-    {
-        public StringBuilder Debug { get; set; } = new();
-        public StringBuilder Info { get; set; } = new();
-        public StringBuilder Error { get; set; } = new();
-
-        public CmdPromptLogger()
-        {
-            WriteDebug = d => Debug.AppendLine(d);
-            WriteInfo = i => Info.AppendLine(i);
-            WriteError = e => Error.AppendLine(e);
-        }
-
-        public Action<string> WriteDebug { get; set; }
-        public Action<string> WriteInfo { get; set; }
-        public Action<string> WriteError { get; set; }
-
-        public void Reset()
-        {
-            Debug = new();
-            Info = new();
-            Error = new();
         }
     }
 }
---
+++ DotNet.Basics/Win/CmdPromptLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace DotNet.Basics.Win
+{
+    public class CmdPromptLogger
+    {
+        public delegate void DebugLoggedEventHandler(string line);
+        public delegate void InfoLoggedEventHandler(string line);
+        public delegate void ErrorLoggedEventHandler(string line);
+
+        public event DebugLoggedEventHandler? DebugLogged;
+        public event InfoLoggedEventHandler? InfoLogged;
+        public event ErrorLoggedEventHandler? ErrorLogged;
+
+        public StringBuilder Debug { get; set; } = new();
+        public StringBuilder Info { get; set; } = new();
+        public StringBuilder Error { get; set; } = new();
+
+        public CmdPromptLogger()
+        {
+            WriteDebug = d =>
+            {
+                Debug.AppendLine(d);
+                DebugLogged?.Invoke(d);
+            };
+            WriteInfo = i =>
+            {
+                Info.AppendLine(i);
+                InfoLogged?.Invoke(i);
+            };
+            WriteError = e =>
+            {
+                Error.AppendLine(e);
+                ErrorLogged?.Invoke(e);
+            };
+        }
+
+        public Action<string> WriteDebug { get; }
+        public Action<string> WriteInfo { get; }
+        public Action<string> WriteError { get; }
+
+        public void Reset()
+        {
+            Debug = new();
+            Info = new();
+            Error = new();
+        }
+    }
+}
