5f9755174c7e117955b19021485bf3c982b337e7
rzmoz <rzmoz@users.noreply.github.com>
2026-04-28 22:31:54 +0200

    cc

--- DotNet.Basics/Diagnostics/LongRunningOperation.cs
+++ DotNet.Basics/Diagnostics/LongRunningOperation.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace DotNet.Basics.Diagnostics
 {
-    public class LongRunningOperation(string name, string? id = null)
+    public class LongRunningOperation(string name, Func<Task<int>> action)
     {
         public string Name { get; } = name;
-        public string Id { get; } = id ?? Guid.NewGuid().ToString();
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public Func<Task<int>> Action { get; } = action;
         public DateTime StartTime { get; } = DateTime.Now;
         public TimeSpan DurationNow => DateTime.Now - StartTime;
     }
--- DotNet.Basics/Diagnostics/LongRunningOperations.cs
+++ DotNet.Basics/Diagnostics/LongRunningOperations.cs
@@ -1,4 +1,5 @@
-﻿using Humanizer;
+﻿using DotNet.Basics.Sys;
+using Humanizer;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
@@ -12,50 +13,46 @@ namespace DotNet.Basics.Diagnostics
     {
         private readonly ILogger? _loog;
         private readonly ConcurrentDictionary<string, LongRunningOperation> _operations = new();
-        private readonly Timer _timer;
+        private readonly Timer? _timer = null;
 
-        public LongRunningOperations(ILogger? loog)
-        : this(loog, new LongRunningOperationsOptions())
-        { }
-        public LongRunningOperations(ILogger? loog, LongRunningOperationsOptions o)
+        public LongRunningOperations(ILogger? loog, LongRunningOperationsOptions? o)
         {
             _loog = loog;
-            if (o.PingInterval <= TimeSpan.Zero)
-                throw new ArgumentOutOfRangeException(nameof(o.PingInterval), "Must be bigger than 0");
+            o ??= new LongRunningOperationsOptions();
 
-            _timer = new Timer(o.PingInterval.TotalMilliseconds)
+            if (o.PingInterval > TimeSpan.Zero)
             {
-                AutoReset = true,
-                Enabled = true
-            };
-            _timer.Elapsed += _timer_Elapsed;
-            _loog?.Trace("Long running operations initialized with ping interval: {}", o.PingInterval.Humanize());
+                _timer = new Timer(o.PingInterval.TotalMilliseconds)
+                {
+                    AutoReset = true,
+                    Enabled = true
+                };
+                _timer.Elapsed += _timer_Elapsed;
+                _loog?.Trace("Long running operations initialized with ping interval: {int}", o.PingInterval.Humanize());
+            }
         }
 
         private void _timer_Elapsed(object? sender, ElapsedEventArgs e)
         {
-            if (_operations.Any() == false)
-                return;
+            var message = _operations.Values
+                .OrderBy(o => o.StartTime)
+                .Select(o => $"[ {o.Name.Highlight()} has been running for {o.DurationNow.Humanize(precision: 2).Highlight()} ]")
+                .JoinString("\r\n");
 
-            var message = string.Empty;
-            foreach (var operation in _operations.Values.OrderBy(o => o.StartTime))
-                message += $"[ {operation.Name.Highlight()} has been running for {operation.DurationNow.Humanize().Highlight()} ]\r\n";
-
-            _loog?.Debug(message.WithGutter());
+            if (!string.IsNullOrEmpty(message))
+                _loog?.Debug(message.WithGutter());
         }
 
         public async Task<int> StartAsync(string name, Func<Task<int>> action)
         {
-            var operation = new LongRunningOperation(name);
-
+            var operation = new LongRunningOperation(name, action);
             var exitCode = int.MinValue;
+            bool added = _operations.TryAdd(operation.Id, operation);
+
             try
             {
-                if (_operations.TryAdd(operation.Id, operation))
-                    _loog?.Debug("{operationName} started", operation.Name.Highlight());
-                else
-                    _loog?.Error("Failed to start {operationName} with Id: {operationId}", operation.Name.Highlight(), operation.Id);
-                exitCode = await action.Invoke().ConfigureAwait(false);
+                _loog?.Debug("{operationName} started", operation.Name.Highlight());
+                exitCode = await operation.Action.Invoke().ConfigureAwait(false);
                 return exitCode;
             }
             catch (Exception e)
@@ -65,10 +62,10 @@ namespace DotNet.Basics.Diagnostics
             }
             finally
             {
-                if (_operations.TryRemove(operation.Id, out _) == false)
-                    _loog?.Warn("{operationName} not removed from {operatorName} stack :-(", operation.Name, nameof(LongRunningOperations));
-
-                _loog?.Debug("{operationName} finished in {operationDurationNow}", operation.Name.Highlight(), operation.DurationNow.Humanize().Highlight());
+                if (added && !_operations.TryRemove(operation.Id, out _))
+                    _loog?.Error("{operationName} not removed from {operatorName} stack :-(", operation.Name, nameof(LongRunningOperations));
+                else
+                    _loog?.Debug("{operationName} finished in {operationDurationNow}", operation.Name.Highlight(), operation.DurationNow.Humanize().Highlight());
 
                 if (exitCode != 0)
                     _loog?.Critical("{operationName} FAILED with exit code: {exitCode}", operation.Name.Highlight(), exitCode.ToString().Highlight());
