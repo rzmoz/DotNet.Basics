111bf2c9e4a357c03951b96a0d958bd555d30e8d
rzmoz <rzmoz@users.noreply.github.com>
2026-05-08 18:29:59 +0200

    cc

--- DotNet.Basics.Cli/Logging/ANSIExtensions.cs
+++ DotNet.Basics.Cli/Logging/ANSIExtensions.cs
@@ -15,36 +15,19 @@ namespace DotNet.Basics.Cli.Logging
     // ------- Spectre.Console ------- //
     public static class ANSIExtensions
     {
-        private static readonly Func<Progress, Progress> _passThroughProgressFunc = _ => _;
-        private static readonly Func<Status, Status> _passThroughStatusFunc = _ => _;
-        private static readonly Func<LiveDisplay, LiveDisplay> _passThroughLiveDisplayFunc = _ => _;
-
         public static string ProgressDefaultMessage { get; set; } = "Starting...";
-
         public static ConsoleStyleTheme Theme { get; set; } = new DefaultDarkTheme();
 
         // ------- Progress ------- //
-        public static void Progress(this ILogger _, string message, Action<ProgressTask> action)
+        public static void Progress(this ILogger _, string message, Action<ProgressTask> action, Func<Progress, Progress>? init = null)
         {
-            Progress(_, message, 0, _passThroughProgressFunc, action);
+            Progress(_, message, 0, action, init);
         }
-        public static void Progress(this ILogger _, double maxValue, Action<ProgressTask> action)
+        public static void Progress(this ILogger _, double maxValue, Action<ProgressTask> action, Func<Progress, Progress>? init = null)
         {
-            Progress(_, ProgressDefaultMessage, maxValue, _passThroughProgressFunc, action);
+            Progress(_, ProgressDefaultMessage, maxValue, action, init);
         }
-        public static void Progress(this ILogger _, string message, double maxValue, Action<ProgressTask> action)
-        {
-            Progress(_, message, maxValue, _passThroughProgressFunc, action);
-        }
-        public static void Progress(this ILogger _, string message, Func<Progress, Progress> init, Action<ProgressTask> action)
-        {
-            Progress(_, message, 0, init, action);
-        }
-        public static void Progress(this ILogger _, double maxValue, Func<Progress, Progress> init, Action<ProgressTask> action)
-        {
-            Progress(_, ProgressDefaultMessage, maxValue, init, action);
-        }
-        public static void Progress(this ILogger _, string message, double maxValue, Func<Progress, Progress> init, Action<ProgressTask> action)
+        public static void Progress(this ILogger _, string message, double maxValue, Action<ProgressTask> action, Func<Progress, Progress>? init = null)
         {
             InitProgress(init).Start(async ctx =>
             {
@@ -58,28 +41,16 @@ namespace DotNet.Basics.Cli.Logging
                 }
             });
         }
-
-        public static async Task ProgressAsync(this ILogger _, string message, Func<ProgressTask, Task> func)
+        
+        public static async Task ProgressAsync(this ILogger _, string message, Func<ProgressTask, Task> func, Func<Progress, Progress>? init = null)
         {
-            await ProgressAsync(_, message, 0, _passThroughProgressFunc, func);
+            await ProgressAsync(_, message, 0, func, init);
         }
-        public static async Task ProgressAsync(this ILogger _, double maxValue, Func<ProgressTask, Task> func)
+        public static async Task ProgressAsync(this ILogger _, double maxValue, Func<ProgressTask, Task> func, Func<Progress, Progress>? init = null)
         {
-            await ProgressAsync(_, ProgressDefaultMessage, maxValue, _passThroughProgressFunc, func);
+            await ProgressAsync(_, ProgressDefaultMessage, maxValue, func, init);
         }
-        public static async Task ProgressAsync(this ILogger _, string message, double maxValue, Func<ProgressTask, Task> func)
-        {
-            await ProgressAsync(_, message, maxValue, _passThroughProgressFunc, func);
-        }
-        public static async Task ProgressAsync(this ILogger _, string message, Func<Progress, Progress> init, Func<ProgressTask, Task> func)
-        {
-            await ProgressAsync(_, message, 0, init, func);
-        }
-        public static async Task ProgressAsync(this ILogger _, double maxValue, Func<Progress, Progress> init, Func<ProgressTask, Task> func)
-        {
-            await ProgressAsync(_, ProgressDefaultMessage, maxValue, init, func);
-        }
-        public static async Task ProgressAsync(this ILogger _, string message, double maxValue, Func<Progress, Progress> init, Func<ProgressTask, Task> func)
+        public static async Task ProgressAsync(this ILogger _, string message, double maxValue, Func<ProgressTask, Task> func, Func<Progress, Progress>? init = null)
         {
             await InitProgress(init).StartAsync(async ctx =>
             {
@@ -91,10 +62,10 @@ namespace DotNet.Basics.Cli.Logging
                 }
             });
         }
-        private static Progress InitProgress(Func<Progress, Progress> init)
+        private static Progress InitProgress(Func<Progress, Progress>? init)
         {
             var progress = AnsiConsole.Progress().AutoRefresh(true).AutoClear(true);
-            progress = init(progress);
+            progress = (init ?? (_ => { return _; })).Invoke(progress);
             return progress;
         }
         private static (ProgressTask task, CancellationTokenSource cts) InitProgressTask(ProgressContext ctx, string message, double maxValue)
@@ -105,97 +76,91 @@ namespace DotNet.Basics.Cli.Logging
         }
 
         // ------- Status ------- //
-        public static void Status(this ILogger _, string startMessage, Action<StatusContext> action)
-        {
-            Status(_, startMessage, _passThroughStatusFunc, action);
-        }
-        public static void Status(this ILogger _, string startMessage, Func<Status, Status> init, Action<StatusContext> action)
+
+        public static void Status(this ILogger _, string startMessage, Action<StatusContext> action, Func<Status, Status>? init = null)
         {
             InitStatus(startMessage, init).Start(startMessage, action);
         }
 
-        public static async Task StatusAsync(this ILogger _, string startMessage, Func<StatusContext, Task> func)
-        {
-            await StatusAsync(_, startMessage, _passThroughStatusFunc, func);
-        }
-        public static async Task StatusAsync(this ILogger _, string startMessage, Func<Status, Status> init, Func<StatusContext, Task> func)
+        public static async Task StatusAsync(this ILogger _, string startMessage, Func<StatusContext, Task> func, Func<Status, Status>? init = null)
         {
             await InitStatus(startMessage, init).StartAsync(startMessage, func);
         }
-        private static Status InitStatus(string startMessage, Func<Status, Status> init)
+        private static Status InitStatus(string startMessage, Func<Status, Status>? init)
         {
             var status = AnsiConsole.Status().AutoRefresh(true);
-            status = init(status);
+            status = (init ?? (_ => { return _; })).Invoke(status);
             return status;
         }
 
         // ------- Live ------- //
-        public static void Live(this ILogger _, IRenderable renderable, Action<LiveDisplayContext> action)
-        {
-            Live(_, renderable, _passThroughLiveDisplayFunc, action);
-        }
-        public static void Live(this ILogger _, IRenderable renderable, Func<LiveDisplay, LiveDisplay> init, Action<LiveDisplayContext> action)
+
+        public static void Live(this ILogger _, IRenderable renderable, Action<LiveDisplayContext> action, Func<LiveDisplay, LiveDisplay>? init = null)
         {
             InitLive(renderable, init).Start(action);
         }
-
-        public static async Task LiveAsync(this ILogger _, IRenderable renderable, Func<LiveDisplayContext, Task> func)
-        {
-            await LiveAsync(_, renderable, _ => { return _; }, func);
-        }
-        public static async Task LiveAsync(this ILogger _, IRenderable renderable, Func<LiveDisplay, LiveDisplay> init, Func<LiveDisplayContext, Task> func)
+        public static async Task LiveAsync(this ILogger _, IRenderable renderable, Func<LiveDisplayContext, Task> func, Func<LiveDisplay, LiveDisplay>? init = null)
         {
             await InitLive(renderable, init).StartAsync(func);
         }
-        private static LiveDisplay InitLive(IRenderable renderable, Func<LiveDisplay, LiveDisplay> init)
+        private static LiveDisplay InitLive(IRenderable renderable, Func<LiveDisplay, LiveDisplay>? init)
         {
             var live = AnsiConsole.Live(renderable).AutoClear(true);
-            live = init(live);
+            live = (init ?? (_ => { return _; })).Invoke(live);
             return live;
         }
 
         // ------- Force Write ------- //
-        public static void ForceWriteLineJson(this ILogger log, object o, Func<JsonText, JsonText>? init = null)
+        public static ILogger ForceWriteLineJson(this ILogger _, object o, Func<JsonText, JsonText>? init = null)
         {
-            log.ForceWriteLineJson(o.ToJson(), init);
+            return ForceWriteLineJson(_, o.ToJson(), init);
         }
-        public static void ForceWriteLineJson(this ILogger log, string str, Func<JsonText, JsonText>? init = null)
+        public static ILogger ForceWriteLineJson(this ILogger _, string str, Func<JsonText, JsonText>? init = null)
         {
-            log.ForceWriteLine(init?.Invoke(new JsonText(str)) ?? new JsonText(str));
+            return ForceWriteLine(_, init?.Invoke(new JsonText(str)) ?? new JsonText(str));
         }
 
         /// <summary>
         /// Outputs to console regardless of loglevel but in chosen loglevel color scheme
         /// </summary>
-        public static void ForceWriteLine(this ILogger log, string str, LogLevel lvl = LogLevel.Debug, bool isSuccess = false, bool isHighlight = false) => log.ForceWriteLine(str, Theme.GetStyle(lvl, isSuccess, isHighlight));
+        public static ILogger ForceWriteLine(this ILogger _, string str, LogLevel lvl = LogLevel.Debug, bool isSuccess = false, bool isHighlight = false) => ForceWriteLine(_, str, Theme.GetStyle(lvl, isSuccess, isHighlight));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWriteLine(this ILogger log, string str, Style? style) => log.ForceWriteLine(new Text(str, style));
+        public static ILogger ForceWriteLine(this ILogger _, string str, Style? style) => ForceWriteLine(_, new Text(str, style));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWriteLine(this ILogger log, params IEnumerable<(string str, Style? style)> strs) => log.ForceWriteLine(strs.Select(str => new Text(str.str, str.style)));
+        public static ILogger ForceWriteLine(this ILogger _, params IEnumerable<(string str, Style? style)> strs) => ForceWriteLine(_, strs.Select(str => new Text(str.str, str.style)));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWriteLine(this ILogger log, params IEnumerable<IRenderable> renderables) => renderables.Append(Text.NewLine).ForEach(AnsiConsole.Write);
+        public static ILogger ForceWriteLine(this ILogger _, params IEnumerable<IRenderable> renderables)
+        {
+            ForceWrite(_, renderables);
+            ForceWrite(_, Text.NewLine);
+            return _;
+        }
 
         /// <summary>
         /// Outputs to console regardless of loglevel but in chosen loglevel color scheme
         /// </summary>
-        public static void ForceWrite(this ILogger log, string str, LogLevel lvl = LogLevel.Debug, bool isSuccess = false, bool isHighlight = false) => log.ForceWrite(str, Theme.GetStyle(lvl, isSuccess, isHighlight));
+        public static ILogger ForceWrite(this ILogger _, string str, LogLevel lvl = LogLevel.Debug, bool isSuccess = false, bool isHighlight = false) => ForceWrite(_, str, Theme.GetStyle(lvl, isSuccess, isHighlight));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWrite(this ILogger log, string str, Style? style) => log.ForceWrite(new Text(str, style));
+        public static ILogger ForceWrite(this ILogger _, string str, Style? style) => ForceWrite(_, new Text(str, style));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWrite(this ILogger log, params IEnumerable<(string str, Style? style)> strs) => log.ForceWrite(strs.Select(s => new Text(s.str, s.style)));
+        public static ILogger ForceWrite(this ILogger _, params IEnumerable<(string str, Style? style)> strs) => ForceWrite(_, strs.Select(s => new Text(s.str, s.style)));
         /// <summary>
         /// Outputs to console regardless of loglevel
         /// </summary>
-        public static void ForceWrite(this ILogger log, params IEnumerable<IRenderable> renderables) => renderables.ForEach(AnsiConsole.Write);
+        public static ILogger ForceWrite(this ILogger _, params IEnumerable<IRenderable> renderables)
+        {
+            renderables.ForEach(AnsiConsole.Write);
+            return _;
+        }
     }
 }
